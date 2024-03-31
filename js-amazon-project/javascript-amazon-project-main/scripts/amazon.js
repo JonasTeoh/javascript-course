ef1b1192b83160a4540ccf685d8956c66d2f3a5c
Jonas Teoh <121472048+JonasTeoh@users.noreply.github.com>
2024-03-31 12:02:08 +0800

    Use data js file

--- js-amazon-project/javascript-amazon-project-main/scripts/amazon.js
+++ js-amazon-project/javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-const products = [{
+/*const products = [{
     image: `images/products/athletic-cotton-socks-6-pairs.jpg`,
     name: `Black and Gray Athletic Cotton Socks - 6 Pairs`,
     rating: {
@@ -30,7 +30,23 @@ const products = [{
         count: 2197
     },
     priceCents: 1899
-}]
+},{
+    image: `images/products/6-piece-white-dinner-plate-set.jpg`,
+    name: `6 Piece White Dinner Plate Set`,
+    rating: {
+        stars: 4,
+        count: 37
+    },
+    priceCents: 2067
+},{
+    image: `images/products/6-piece-non-stick-baking-set.webp`,
+    name: `6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set`,
+    rating: {
+        stars: 5,
+        count: 2197
+    },
+    priceCents: 1899
+}]*/
 
 let productsHTML = '';
 
